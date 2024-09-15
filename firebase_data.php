1740732ef12a6d5dbe3d562ac8f9bfdecc41924a
Vishvaa <vishvaalogeswaran@gmail.com>
2024-09-15 15:40:40 +0800

    City details full re-design

--- firebase_data.php
+++ firebase_data.php
@@ -11,4 +11,20 @@ $reference = $database->getReference('Packages/' . $country);
 $snapshot = $reference->getSnapshot();
 $dataCity = $snapshot->getValue();
 
+//data for all city images
+$city = isset($_GET['city']) ? $_GET['city'] : '';
+
+$reference = $database->getReference('Packages/' . $country . '/' . $city . '/Images');  
+$snapshot = $reference->getSnapshot();
+$dataCityImages = $snapshot->getValue();
+
+//data for all city itineraries
+$reference = $database->getReference('Packages/' . $country . '/' . $city . '/Itinerary');  
+$snapshot = $reference->getSnapshot();
+$dataCityItinerary = $snapshot->getValue();
+
+//data for all city hotels
+$reference = $database->getReference('Packages/' . $country . '/' . $city . '/Hotels');  
+$snapshot = $reference->getSnapshot();
+$dataHotels = $snapshot->getValue();
 ?>
