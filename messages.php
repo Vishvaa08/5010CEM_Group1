e4e56b9b98cd973c741a7efb02c58562056ed50b
Sarannia2306 <90039978+Sarannia2306@users.noreply.github.com>
2024-10-24 22:39:27 +0800

    current updates

--- messages.php
+++ messages.php
@@ -25,7 +25,7 @@ $messages = $snapshot->getValue();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Messages</title>
-    <link rel="stylesheet" href="messages.css"> 
+    <link rel="stylesheet" href="css/messages.css"> 
 </head>
 <body>
 <div class="sidebar">
